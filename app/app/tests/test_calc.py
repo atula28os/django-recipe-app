1cd5eb857fd6ef274a17a8b5f09e354711cd66e8
atula28os <atula28os@gmail.com>
2023-12-31 21:05:52 +0530

    Sample Test Case

---
+++ app/app/tests/test_calc.py
@@ -0,0 +1,20 @@
+"""Django Test Module in Root App """
+
+from django.test import SimpleTestCase
+
+from app import calc # type: ignore
+
+class CalcTests(SimpleTestCase):
+
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        res = calc.minus(6, 5)
+        self.assertEqual(res, 1)
+
+
